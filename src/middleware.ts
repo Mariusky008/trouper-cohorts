e418cdd0270f30c88bdbc58b99f72be71021b9cd
Mariusky008 <mariusky008@gmail.com>
2026-02-08 15:39:19 +0100

    fix: force middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+  // Update session for Supabase Auth
   return await updateSession(request);
 }
 
